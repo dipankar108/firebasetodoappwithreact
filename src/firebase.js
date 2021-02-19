63078c84475e224bc9ac86b2e69b8b13583d15d2
Dipankar Debnath <dipankar0debnath@gmail.com>
2021-02-18 23:13:59 -0800

    hosting to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 const firebaseApp = firebase.initializeApp(
-    { apiKey: "AIzaSyCi3EClQVqQ5qS35cTXgG7yw1zamCVQ6ZA",
-  authDomain: "fire-base-todo-app-with-react.firebaseapp.com",
+    { apiKey: "firebase",
+  authDomain: "firebase",
   projectId: "fire-base-todo-app-with-react",
   storageBucket: "fire-base-todo-app-with-react.appspot.com",
-  messagingSenderId: "680889060183",
-  appId:"1:680889060183:web:4184a8226e514966ddcc12",
-  measurementId: "G-W8YS5XPKBJ"}
+  messagingSenderId: "sfs89060183",
+  appId:"1:680889060sfsdf184a8226e514966ddcc12",
+  measurementId: "G-W8YS5adfsafJ"}
 )
 const db = firebaseApp.firestore();
 export default db ;
